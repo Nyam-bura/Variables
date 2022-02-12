2ac28648c0bf70ef485267f57d6679df190ed83f
Nyam-bura <nyamburaajanee@gmail.com>
2022-02-12 14:53:22 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main() {
+var name = "jane nyambura"
+println(name)
+var age = "20"
+println(age)
+var phonenumber = "0790500842"
+println(phonenumber)
+var weight = "53"
+println(weight)
+val  iskenyan: Boolean = true
+println(iskenyan) // output true
+val isnotkenyan: Boolean = false
+println(isnotkenyan) // = output false
+
+
+
+
+
+
+}
